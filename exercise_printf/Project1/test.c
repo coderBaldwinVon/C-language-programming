d6b1eff0aab63bc03eed6933f36278f99ca66cda
baldwinvon <baldwin.von@outlook.com>
2022-12-08 21:08:51 +0800

    This is the exercise for printf function.

---
+++ exercise_printf/Project1/test.c
@@ -0,0 +1,33 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+////judge whether someone is a genius or not
+//
+//int main()
+//{
+//    int i = 0;
+//    while (scanf("%d", &i) != EOF)
+//    {
+//        if (i >= 140)
+//        {
+//            printf("Genius\n");
+//        }
+//    }
+//    return 0;
+//}
+//
+////help Kiki release an announcement
+//int main()
+//{
+//    printf("I lost my cellphone!\n");
+//    return 0;
+//}
+//
+//
+////calculate the result of"(-8 + 22) * a - 10 + c / 2)"
+//
+//int main()
+//{
+//    int a = 40;
+//    int c = 212;
+//    printf("%d\n", (-8 + 22) * a - 10 + c / 2);
+//}
