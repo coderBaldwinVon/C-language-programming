3279cdc47b4260584664909f58c3a0e6f497972b
baldwinvon <baldwin.von@outlook.com>
2022-12-09 23:00:59 +0800

    This is the exercise for escape character.

---
+++ exercise_Escape character/exercise_Escape character/test.c
@@ -0,0 +1,35 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("The size of short is %ld bytes.\n", sizeof(short));
+//	printf("The size of int is %ld bytes.\n", sizeof(int));
+//	printf("The size of long is %ld bytes.\n", sizeof(long));
+//	printf("The size of long long is %ld bytes.\n", sizeof(long long));
+//	return 0;
+//}
+//
+//
+////transfer decimal integer into the octal and hexadecimal(uppercase letters)
+//int main()
+//{
+//	printf("0%o 0X%X\n", 1234, 1234);
+//	return 0;
+//}
+//
+//
+////reverse output a four digit number
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int ret = 0;
+//    while (n)
+//    {
+//        printf("%d", n % 10);
+//        n = n / 10;
+//    }
+//    return 0;
+//}
+//
+//
