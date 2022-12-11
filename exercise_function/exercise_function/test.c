cab5758b0851ab7ab6f4a8de360264862c8ab492
baldwinvon <baldwin.von@outlook.com>
2022-12-11 22:33:06 +0800

    This is the exercise for function.

---
+++ exercise_function/exercise_function/test.c
@@ -0,0 +1,66 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//
+//int Max(int x, int y)
+//{
+//	return(x > y ? x : y);
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int m = Max(a, b);
+//	printf("%d\n", m);
+//	return 0;
+//}
+//
+//
+//int main() {
+//    int a = 0;
+//    int b = 0;
+//    while (scanf("%d %d", &a, &b) == 2)
+//    {
+//        if (a > b)
+//            printf("%d>%d\n", a, b);
+//        if (a == b)
+//            printf("%d=%d\n", a, b);
+//        if (a < b)
+//            printf("%d<%d\n", a, b);
+//    }
+//
+//    return 0;
+//}
+//
+//
+//
+//int main() {
+//    int a = 0;
+//    while (scanf("%d", &a) == 1)
+//    {
+//        if (a % 2 == 0)
+//            printf("Even\n");
+//        else
+//            printf("Odd\n");
+//    }
+//    return 0;
+//}
+//
+//
+//
+//int main()
+//{
+//    int a = 0;
+//    while (scanf("%d", &a) == 1)
+//        a >= 60 ? printf("Pass\n") : printf("Fail\n");
+//    return 0;
+//}
+//
+//
+//int main() {
+//    int a = 0;
+//    while (scanf("%d\n", &a) == 1)
+//        if (a >= 90)
+//            printf("Perfect\n");
+//    return 0;
+//}
